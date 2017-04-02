47bc17ebc644b74fd8c60613a02c9de622fd07ab
Rhys Davies <rhys@johnguant.com>
2017-04-03 02:05:47 +1200

    Fireworks on change

--- src/com/kwiius/placemc/PlaceBlocksTask.java
+++ src/com/kwiius/placemc/PlaceBlocksTask.java
@@ -1,8 +1,9 @@
 package com.kwiius.placemc;
 
-import org.bukkit.ChatColor;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Firework;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -34,6 +35,17 @@ public class PlaceBlocksTask extends BukkitRunnable {
             if (p.definitelyNew || b.getData() != p.blockType) {
                 b.setData(p.blockType);
                 updated++;
+                Firework f = (Firework) world.spawn(new Location(world, p.x, 30, p.y), Firework.class);
+                FireworkMeta fm = f.getFireworkMeta();
+                fm.addEffect(FireworkEffect.builder()
+                        .flicker(false)
+                        .trail(true)
+                        .with(FireworkEffect.Type.BALL)
+                        .withColor(DyeColor.getByWoolData(p.blockType).getColor())
+                        .withFade(DyeColor.getByWoolData(p.blockType).getColor())
+                        .build());
+                fm.setPower(3);
+                f.setFireworkMeta(fm);
             }
         }
 
